5508623d920a77f8baa5cc01dba469b74362121c
jamiebirch <jamie.d.birch@gmail.com>
2017-07-31 01:46:03 +0300

    Primality fix

--- src/main/Primality.java
+++ src/main/Primality.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Primality {
 
     public static boolean isPrime(int n){
-        for (int i = 2; i < (int)Math.sqrt(n); i++){
+        if(n < 2){
+            return false;
+        }
+        for (int i = 2; i <= (int)(Math.sqrt(n)); i++){
             if (n % i == 0) return false;
         }
         return true;
