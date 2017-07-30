3187adf9203db198f7a90ed6675492baa9087074
jamiebirch <jamie.d.birch@gmail.com>
2017-07-30 23:28:29 +0300

    Comparator fix

--- src/main/SortingComparator.java
+++ src/main/SortingComparator.java
@@ -27,7 +27,7 @@ public class SortingComparator {
                 return -1;
             } else if (p1.score < p2.score){
                 return 1;
-            } else return 0;
+            } else return p1.name.compareTo(p2.name);
         }
     }
 
